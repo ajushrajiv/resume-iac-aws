510e250512e91e278428ed6714e427e4f86a26fc
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-09-20 22:13:06 +0200

    format

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -21,13 +21,13 @@ resource "aws_s3_bucket_policy" "access_policy_for_s3" {
     "Statement" : [
       {
         "Effect" : "Allow",
-        "Principal" : { "AWS": "arn:aws:iam::144130276541:root" },
+        "Principal" : { "AWS" : "arn:aws:iam::144130276541:root" },
         "Action" : "s3:ListBucket",
         "Resource" : "arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}"
       },
       {
         "Effect" : "Allow",
-        "Principal" : { "AWS": "arn:aws:iam::144130276541:root" },
+        "Principal" : { "AWS" : "arn:aws:iam::144130276541:root" },
         "Action" : ["s3:GetObject", "s3:PutObject"],
         "Resource" : "arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"
       }
