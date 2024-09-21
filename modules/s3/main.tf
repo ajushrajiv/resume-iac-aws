3293e0f26693993c8e67ae9af7d5fcc2b7a423f9
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-09-21 06:34:11 +0200

    s3 bucket with random name

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name
+  bucket = "matchmyresume-backend-state-demo--${var.git_sha}"
 
   tags = {
     Name        = "resume-backend-state"
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,13 +1,9 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket to store the Terraform state"
-  type        = string
-}
-
 variable "aws_region" {
   description = "AWS region to deploy"
   default     = "eu-central-1"
 }
 
 variable "git_sha" {
-  type = string
+  description = "The random name of the S3 bucket generated to store the Terraform state"
+  type        = string
 }
