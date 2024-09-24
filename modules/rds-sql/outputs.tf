6b9b8fe940963673603598053f1d7ae2e3e844fd
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-09-24 10:57:42 +0200

    rds-backend

--- modules/rds-sql/outputs.tf
+++ modules/rds-sql/outputs.tf
@@ -6,8 +6,14 @@ output "rds_db_name" {
   value = aws_db_instance.resume_db_instance.db_name
 }
 
-output "rds_private_ip" {
+output "rds_private_address" {
   description = "The private IP address of the RDS instance"
   value       = aws_db_instance.resume_db_instance.address
 }
+
+output "rds_private_ip" {
+  description = "The private IP address of the RDS instance"
+  value       = aws_db_instance.resume_db_instance.id
+}
+
 
