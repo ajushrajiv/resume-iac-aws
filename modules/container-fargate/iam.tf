fef8ebae30b3657f29f5923cbc395f65ee5e162f
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-09-24 17:47:49 +0200

    iam role

--- modules/container-fargate/iam.tf
+++ modules/container-fargate/iam.tf
@@ -1,10 +1,6 @@
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-}
-
+# ECS Task Execution Role
 resource "aws_iam_role" "ecs_task_execution_role" {
-  count = try(length(data.aws_iam_role.ecs_task_execution_role.id), 0) == 0 ? 1 : 0
-  name  = "ecsTaskExecutionRole"
+  name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -19,3 +15,36 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     ]
   })
 }
+
+# ECS Task Execution Policy (Attach this to the execution role)
+resource "aws_iam_policy_attachment" "ecs_task_execution_role_policy" {
+  name       = "ecsTaskExecutionRolePolicy"
+  roles      = [aws_iam_role.ecs_task_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Task Role (For containers to assume permissions, like accessing RDS, S3, etc.)
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsTaskRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# attach policies to the task role if your container needs to access other AWS services (e.g., RDS, S3)
+resource "aws_iam_policy_attachment" "ecs_task_role_policy" {
+  name       = "ecsTaskRolePolicy"
+  roles      = [aws_iam_role.ecs_task_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+
