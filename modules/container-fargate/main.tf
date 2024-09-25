23fbd5b68fbf1bea468ec3819cfd59c07b86296a
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-09-25 09:54:09 +0200

    ecs security group

--- modules/container-fargate/main.tf
+++ modules/container-fargate/main.tf
@@ -34,6 +34,27 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
   egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port = 3306
     to_port   = 3306
     protocol  = "tcp"
