7b6d54989008d8d417d11735db8948c74d8f4dd8
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-10-03 10:14:26 +0200

    format

--- modules/amplify/main.tf
+++ modules/amplify/main.tf
@@ -8,14 +8,14 @@ data "terraform_remote_state" "backend-ec2" {
 }
 
 resource "aws_amplify_app" "matchmyresume_amplify_app" {
-  name                = "matchmyresume"
-  repository          = "https://github.com/ajushrajiv/resume-frontend"
-  access_token        = var.access_token_repo
-  
-  platform            = "WEB"
-  
+  name         = "matchmyresume"
+  repository   = "https://github.com/ajushrajiv/resume-frontend"
+  access_token = var.access_token_repo
+
+  platform = "WEB"
+
   environment_variables = {
-    _LIVE_UPDATES = "enabled"
+    _LIVE_UPDATES             = "enabled"
     NEXT_PUBLIC_WEBSOCKET_URL = "ws://${data.terraform_remote_state.backend-ec2.outputs.load_balancer_dns}:5555"
   }
 
@@ -30,8 +30,8 @@ resource "aws_amplify_app" "matchmyresume_amplify_app" {
 }
 
 resource "aws_amplify_branch" "main_branch" {
-  app_id       = aws_amplify_app.matchmyresume_amplify_app.id
-  branch_name  = "development"
+  app_id      = aws_amplify_app.matchmyresume_amplify_app.id
+  branch_name = "development"
 
   enable_auto_build = true
 
